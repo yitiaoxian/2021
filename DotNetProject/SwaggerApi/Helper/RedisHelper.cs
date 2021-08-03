991947162b62d31e883f5b1055cbe7e391534fcc
yitiaoxian <709008281@qq.com>
2021-08-03 16:28:43 +0800

    Redis

---
+++ DotNetProject/SwaggerApi/Helper/RedisHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerApi.Helper
+{
+    public class RedisHelper : IDisposable
+    {
+        //连接字符串
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DotNetProject/SwaggerApi/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerApi.Models
+{
+    /// <summary>
+    /// 产品测试
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id {  get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public double Price {  get; set; }
+    }
+}
